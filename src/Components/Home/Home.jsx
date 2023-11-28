46ffa6ba4524a091ae6d20dde005bbe56da93d88
Pratik778899 <pratik78877@gmail.com>
2023-11-28 11:54:33 +0530

    Added the Project

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className="text-5xl text-center mt-40">
+        <h1>Welcome Bhai</h1>
+    </div>
+  )
+}
+
+export default Home
